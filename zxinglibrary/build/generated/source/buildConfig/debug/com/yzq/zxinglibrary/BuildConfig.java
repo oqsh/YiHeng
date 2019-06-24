c10031d167a0f2e5ca7704284e764760dc63b236
oqsh <30433920+oqsh@users.noreply.github.com>
2019-06-24 18:44:17 +0800

    zxing库

---
+++ zxinglibrary/build/generated/source/buildConfig/debug/com/yzq/zxinglibrary/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yzq.zxinglibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yzq.zxinglibrary";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 227;
+  public static final String VERSION_NAME = "2.2.7";
+}
